4066fe05cd2f75459a216d3fc79b524c522ea4c5
Jaime Terreu <jaime@terreu.com>
2017-05-10 01:14:30 +1000

    Added attendance label to paid courses

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,6 +11,7 @@ class BookingsController < ApplicationController
     @dates = @course.course_dates
     @bookings = current_user.bookings
     @bookings.where(:course_date_id == @course.course_dates)
+    @paid_booking = current_user.bookings.paid_booking
   end
 
   def create
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :course_date
   # has_one :rating, dependent: :destroy
   scope :no_payments, -> {where(payment: false)}
-
+  scope :paid_booking, -> {where(payment: true)}
   #has_one :course, through: :course_date
 
   def price
