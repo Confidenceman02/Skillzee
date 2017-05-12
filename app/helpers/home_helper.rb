563af5c1c20f25384e8a797e96c8936b4544dcc6
Jaime Terreu <jaime@terreu.com>
2017-05-12 23:12:23 +1000

    styling on about, nav, courses and search

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,8 @@ module HomeHelper
   def info_icon
     '<i class="fa fa-info-circle fa-5x" aria-hidden="true"></i>'.html_safe
   end
+
+  def question_icon
+    '<i class="fa fa-question-circle fa-5x" aria-hidden="true"></i>'.html_safe
+  end
 end
