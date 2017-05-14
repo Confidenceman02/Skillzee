ae7c4e162644689da5996321541e24834fdd90ae
Jaime Terreu <jaime@terreu.com>
2017-05-14 21:41:17 +1000

    shrine issue

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,25 +3,25 @@ require "shrine/storage/file_system"
 require "shrine/storage/s3"
 
 s3_options = {
-  access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region:            ENV['S3_REGION'],
-  bucket:            ENV['S3_BUCKET_NAME'],
+    access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+    region:             ENV['S3_REGION'],
+    bucket:             ENV['S3_BUCKET_NAME'],
 }
 
 if Rails.env.development?
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store") # permanent
-  
-}
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
+    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store")#, # permanent
+  }
 else
-Shrine.storages = {
-  cache: Shrine::Storage::S3.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::S3.new(prefix: "store", **s3_options)# production s3
-}
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
+    store: Shrine::Storage::S3.new(prefix: "store", **s3_options) #production S3
+  }
 end
 
-Shrine.plugin :activerecord # or :activerecord
+
+Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for forms
 Shrine.plugin :rack_file # for non-Rails apps
