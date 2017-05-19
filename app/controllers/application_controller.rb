8bb88b96bbdfcea426598c38b771731f6e6b3113
Jaime Terreu <jaime@terreu.com>
2017-05-19 13:07:47 +1000

    only admin can access users page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def require_admin
+    unless current_user && current_user.admin == true
+      flash[:error] = "You are not an admin"
+      redirect_to '/'
+    end
+  end
 
   private
 
